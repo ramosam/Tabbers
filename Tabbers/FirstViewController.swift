7b3e19d0fe3e76f84f5ca06c03c7b8084f6ece77
Alicia Ramos <ramosam@my.lanecc.edu>
2020-02-21 18:18:22 -0800

    set constraints on firstViewController

--- Tabbers/FirstViewController.swift
+++ Tabbers/FirstViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    private let numberOptions = ["0", "1", "2", "3", "4", "5"]
 
+    @IBOutlet weak var picker1: UIPickerView!
+    @IBOutlet weak var picker2: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return numberOptions[row]
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        picker1.dataSource = (numberOptions as! UIPickerViewDataSource)
+        picker2.dataSource = (numberOptions as! UIPickerViewDataSource)
+        
     }
 
 
